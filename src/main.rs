975e870494824fbcc356c87dc8fea46f36b96e87
Dominic Maluski <dominic.maluski@gmail.com>
2023-03-25 15:01:23 -0700

    expanding functionality

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,45 @@ use serde::{Serialize, Deserialize};
 
 #[tokio::main]
 async fn main() {
+    let mut stocks: Vec<StockJson> = Vec::new();
     let api_key = "1FGPYOV8MJGHJ1IC";
+
+    parse_args(std::env::args());
+
+    // replacing this with parse_args
     let symbol = match std::env::args().skip(1).next() {
         Some(arg) => arg,
         None => panic!("No argument given")
-    }; 
+    };
+    // end code to be replaced
+    
     let url = format!("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={}&apikey={}", symbol, api_key);
-    let stock = get_stock(url)
-        .await;
+    let stock = match get_stock(url).await {
+        Ok(s) => s,
+        Err(_) => panic!("Fetching stock from API failed")
+    };
+    stocks.push(stock);
+    display_stocks(stocks);
+}
 
-    println!("Printing from main:\n{:#?}\n", stock);
+fn parse_args(args: std::env::Args) {
+    // return an enum with info about necessary follow up?
+}
+
+fn display_stocks(stocks: Vec<StockJson>) {
+    println!("Hello from display_stocks");
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Stock {
+struct StockJson {
     #[serde(rename = "Global Quote")]
     quote: HashMap<String, String>
 }
 
-async fn get_stock(url: String) -> Result<Stock, Box<(dyn std::error::Error)>> {
+async fn get_stock(url: String) -> Result<StockJson, Box<(dyn std::error::Error)>> {
     
     println!("Printing from get_stock: {}", url);
-    let stock: Stock = reqwest::Client::new()
+    let stock: StockJson = reqwest::Client::new()
         .get(url)
         .send()
         .await?
