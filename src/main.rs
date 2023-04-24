f22427ea4e1225f7a58208c3bad6842b3932ee93
Dominic Maluski <dominic.maluski@gmail.com>
2023-04-24 14:55:15 -0700

    Significant progress on new API. Help functionality added. Ability to pass all arguments added, but functionality not quite there yet.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 extern crate reqwest;
 extern crate tokio;
 mod stocks;
-use std::io::{Error,ErrorKind};
-use std::io::prelude::*;
 use std::fs::OpenOptions;
-use stocks::{display_stocks, get_stock, StockJson};
+use std::io::prelude::*;
+use std::io::{Error, ErrorKind};
+use stocks::{display_stocks, get_stock_alpha, get_stock_rapid, StockJsonA, StockJsonR};
 
 /// Branch is an enum which branches the main function depending upon
 /// the arguments passed to the program. If an argument is followed by
@@ -14,57 +14,61 @@ enum Branch {
     Add(Vec<String>),
     Remove(Vec<String>),
     List,
-    None
+    None,
 }
-
 
 const SNIP0: &str = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=";
 const SNIP1: &str = "&apikey=1FGPYOV8MJGHJ1IC";
+const USAGE: &str = "Usage:\n\tcharts-rs <symbol>\n\
+                 \tcharts-rs add <symbol>\n\
+                 \tcharts-rs rm <symbol>\n\
+                 \tcharts-rs list\n\
+                 \tcharts-rs list -s  <list name>\n\
+                 \tcharts-rs list -n <list name>\n\
+                 \tcharts-rs list -d <list name>\n\
+                 \tcharts-rs list -a\n\n\
+                 Enter charts-rs --help for more information.";
+const HELP: &str =
+    "Help:\n\tcharts-rs <symbol>\tEnter a list of symbols to display more than one.\n\
+                 \tcharts-rs add <symbol>\tAdds a symbol or list of symbols to the watchlist.\n\
+                 \tcharts-rs rm <symbol>\tRemoves a symbol or list of symbols from the watchlist.\n\
+                 \tcharts-rs list\t\tLists the data for all symbols currently in the list.\n\
+                 \tcharts-rs list -s <list name>\tSwitches the current list to <list name>\n\
+                 \tcharts-rs list -n <list name>\tCreates a new empty list named <list name>\n\
+                 \tcharts-rs list -d <list name>\tDeletes the list named <list name>\n\
+                 \tcharts-rs list -a\t\tLists the names of all currently created lists\n";
 
 #[tokio::main]
 async fn main() {
-    
     let mut args: Vec<String> = std::env::args().skip(1).collect();
     let stocks = match parse_args(&mut args) {
-        Branch::Symbol(symbols) => {
-            retrieve_list(symbols).await
-        },
-        Branch::Add(symbols) => {
-            // Append symbols received as arguments to add command to the list
-            match append_list(symbols) {
-                Ok(()) => None,
-                Err(_) => {
-                    println!("Error appending symbols to list.\n");
-                    None
-                }
+        Branch::Symbol(symbols) => retrieve_list(symbols).await,
+        Branch::Add(symbols) => match append_list(symbols) {
+            Ok(()) => None,
+            Err(_) => {
+                println!("Error appending symbols to list.\n");
+                None
             }
         },
-        Branch::Remove(symbols) => {
-            // Attempt to remove symbols received as arguments to remove 
-            // command from list file.
-            match edit_list(symbols) {
-                Ok(()) => None,
-                Err(e) => {
-                    println!("Error: {e}\n");
-                    None
-                }
-            }
-        }
-        Branch::List => {
-            match read_list() {
-                Some(list) => retrieve_list(list).await,
-                None => {
-                    println!("List not found...\n");
-                    None
-                }
+        Branch::Remove(symbols) => match edit_list(symbols) {
+            Ok(()) => None,
+            Err(e) => {
+                println!("Error: {e}\n");
+                None
             }
         },
-        Branch::None => {
-            println!("Usage:\n\tcharts-rs <symbol>\n\tcharts-rs add <symbol>\n\tcharts-rs list\n\tcharts-rs rm <symbol>");
-            None
-        }
+        Branch::List => match read_list() {
+            Some(list) => retrieve_list(list).await,
+            None => {
+                println!("List not found...\n");
+                None
+            }
+        },
+        Branch::None => None,
     };
-    if stocks.is_some() { display_stocks(stocks.unwrap()); }
+    if stocks.is_some() {
+        display_stocks(stocks.unwrap());
+    }
 }
 
 /// Function that takes command line arguments and returns a Branch.
@@ -73,36 +77,67 @@ async fn main() {
 
 fn parse_args(args: &mut Vec<String>) -> Branch {
     if args.len() == 0 {
+        println!("{USAGE}");
         return Branch::None;
-    } 
+    }
     match &*args[0] {
+        "--help" => {
+            println!("{HELP}");
+            Branch::None
+        }
         "add" => {
             args.swap_remove(0);
             Branch::Add(args.clone())
-        },
+        }
         "rm" => {
             args.swap_remove(0);
             Branch::Remove(args.clone())
-        },
-        "list" => Branch::List,
-        _ => {
-            Branch::Symbol(args.clone())
-        } 
+        }
+        "list" => {
+            if args.len() > 3 {
+                println!("Error: Invalid syntax.");
+                return Branch::None;
+            }
+            // perhaps wrap an enum inisde of Branch::List?
+            // is enumception too much? I don't want to put
+            // functionality inside the parse_args function.
+            match &*args[1] {
+                "-s" => {
+                    println!("TODO: list -s");
+                    Branch::None
+                }
+                "-n" => {
+                    println!("TODO: list -n");
+                    Branch::None
+                }
+                "-d" => {
+                    println!("TODO: list -d");
+                    Branch::None
+                }
+                "-a" => {
+                    println!("TODO: list -a");
+                    Branch::None
+                }
+                _ => Branch::List,
+            }
+        }
+        _ => Branch::Symbol(args.clone()),
     }
 }
 
-
 fn append_list(symbols: Vec<String>) -> Result<(), Error> {
-    let mut list = OpenOptions::new().append(true).create(true).open("list.txt")?;
+    let mut list = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("list.txt")?;
     for symbol in symbols {
         list.write(symbol.as_bytes())?;
         list.write(b"\t")?;
-    } 
+    }
     Ok(())
 }
 
 fn edit_list(symbols: Vec<String>) -> Result<(), Error> {
-    
     let new_list = match read_list() {
         Some(mut current_list) => {
             for del_item in symbols {
@@ -111,8 +146,9 @@ fn edit_list(symbols: Vec<String>) -> Result<(), Error> {
                     if current_list[i] == del_item {
                         current_list.swap_remove(i);
                         i = current_list.len();
+                    } else {
+                        i += 1;
                     }
-                    else { i += 1; }
                 }
             }
             Some(current_list)
@@ -122,11 +158,14 @@ fn edit_list(symbols: Vec<String>) -> Result<(), Error> {
     match new_list {
         None => Err(Error::new(ErrorKind::InvalidData, "File was empty!")),
         Some(l) => {
-            let mut list = OpenOptions::new().write(true).truncate(true).open("list.txt")?;
+            let mut list = OpenOptions::new()
+                .write(true)
+                .truncate(true)
+                .open("list.txt")?;
             for symbol in l {
                 list.write(symbol.as_bytes())?;
                 list.write(b"\t")?;
-            } 
+            }
             Ok(())
         }
     }
@@ -137,37 +176,36 @@ fn read_list() -> std::option::Option<Vec<String>> {
         Ok(f) => f,
         Err(e) => {
             println!("Error {e} opening file.");
-            return None
+            return None;
         }
     };
     let mut s = String::new();
     let contents: Option<&str> = match list.read_to_string(&mut s) {
         Ok(0) => None,
         Ok(_) => Some(s.as_str()),
-        Err(e) => panic!("Error {e} reading from file.")
+        Err(e) => panic!("Error {e} reading from file."),
     };
     match contents {
         Some(slice) => {
             let mut parsed_contents: Vec<String> = Vec::new();
             for item in slice.split('\t') {
-                if item != "" { parsed_contents.push(item.to_owned()); }
+                if item != "" {
+                    parsed_contents.push(item.to_owned());
+                }
             }
             Some(parsed_contents)
-        },
-        None => None
+        }
+        None => None,
     }
 }
 
-
-async fn retrieve_list(list: Vec<String>) -> std::option::Option<Vec<StockJson>> {
-    let mut out: Vec<StockJson> = Vec::new();
+async fn retrieve_list(list: Vec<String>) -> std::option::Option<Vec<StockJsonA>> {
+    let mut out: Vec<StockJsonA> = Vec::new();
     for symbol in list {
         println!("{symbol}");
         let url = format!("{}{}{}", SNIP0, symbol, SNIP1);
-        match get_stock(url).await {
-            Ok(s) => {
-                out.push(s)
-            },
+        match get_stock_alpha(url).await {
+            Ok(s) => out.push(s),
             Err(e) => {
                 println!("{e}");
             }
--- src/stocks.rs
+++ src/stocks.rs
@@ -1,33 +1,23 @@
 use reqwest;
-use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 use serde_json;
-use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
 
-pub fn display_stocks(stocks: Vec<StockJson>) {
-
+pub fn display_stocks(stocks: Vec<StockJsonA>) {
     let dash = '-';
-    let headers = [
-        " Symbol ",
-        " Price  ",  
-        " Prev   ",
-        " Change ",
-        " Pct %  "
-    ];
+    let headers = [" Symbol ", " Price  ", " Prev   ", " Change ", " Pct %  "];
 
     let header_map = HashMap::from([
-        (headers[0],  "01. symbol"),
-        (headers[1],  "05. price"),
-        (headers[2],  "08. previous close"),
-        (headers[3],  "09. change"),
-        (headers[4],  "10. change percent")
+        (headers[0], "01. symbol"),
+        (headers[1], "05. price"),
+        (headers[2], "08. previous close"),
+        (headers[3], "09. change"),
+        (headers[4], "10. change percent"),
     ]);
 
-    print!("\t{}\t{}\t{}\t{}\t{}\n",
-        headers[0],
-        headers[1],
-        headers[2],
-        headers[3],
-        headers[4]
+    print!(
+        "\t{}\t{}\t{}\t{}\t{}\n",
+        headers[0], headers[1], headers[2], headers[3], headers[4]
     );
 
     for item in headers {
@@ -43,24 +33,24 @@ pub fn display_stocks(stocks: Vec<StockJson>) {
         let default = String::from("N/A");
         for i in 0..headers.len() {
             s.push(' ');
-            let key = header_map
-                .get(headers[i])
-                .unwrap()
-                .to_string();
-            let mut value = stock.quote
-                .get(&key)
-                .unwrap_or(&default)
-                .to_owned();
+            let key = header_map.get(headers[i]).unwrap().to_string();
+            let mut value = stock.quote.get(&key).unwrap_or(&default).to_owned();
             if i > 0 && i < 4 {
-                for _ in 0..2 { value.pop(); }
+                for _ in 0..2 {
+                    value.pop();
+                }
             }
             if i == 4 {
-                for _ in 0..3 { value.pop(); }
+                for _ in 0..3 {
+                    value.pop();
+                }
                 value.push('%');
             }
             s.push_str(&value);
             if value.len() < 7 {
-                for _ in 0..(7 - value.len()) { s.push(' '); }
+                for _ in 0..(7 - value.len()) {
+                    s.push(' ');
+                }
             }
             s.push('\t');
         }
@@ -69,21 +59,56 @@ pub fn display_stocks(stocks: Vec<StockJson>) {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct StockJson {
+pub struct StockJsonA {
     #[serde(rename = "Global Quote")]
-    quote: HashMap<String, String>
+    quote: HashMap<String, String>,
 }
 
-pub async fn get_stock(url: String) -> Result<StockJson, Box<reqwest::Error>> {
+#[derive(Debug, Serialize, Deserialize)]
+pub struct StockJsonR {
+    price: f32,
+    change_point: f32,
+    change_percentage: f32,
+    total_vol: String,
+}
+
+/// Multiple implementations of get_stock to account for different
+/// APIs. This uses the Alphavantage API, which sadly only allows
+/// for 5 requests per minute. Its response time is quite fast though.
+
+pub async fn get_stock_alpha(url: String) -> Result<StockJsonA, Box<reqwest::Error>> {
+    let res = reqwest::Client::new().get(url).send().await?.text().await?;
+    println!("Response: {}", res);
+    let stock: StockJsonA = serde_json::from_str(&res).unwrap_or(StockJsonA::from(StockJsonA {
+        quote: HashMap::new(),
+    }));
+    Ok(stock)
+}
+
+/// Implementation for the 'realstonks' API on Rapidapi. Main benefit of this API is
+/// that it's free and has nearly unlimited requests. Response time is a
+/// little slower, however.
+
+pub async fn get_stock_rapid(symbol: String) -> Result<StockJsonR, Box<reqwest::Error>> {
+    let url = format!("https://realstonks.p.rapidapi.com/{}", symbol);
     let res = reqwest::Client::new()
-        .get(url)
+        .request(reqwest::Method::GET, url)
+        .header(reqwest::header::CONTENT_TYPE, "application/octet-stream")
+        .header(
+            "X-RapidAPI-Key",
+            "216c8810b8msh81fd3895966c048p1f50b6jsn9dbb47c8f68e",
+        )
+        .header("X-RapidAPI-Host", "realstonks.p.rapidapi.com")
         .send()
         .await?
         .text()
         .await?;
     println!("Response: {}", res);
-    let stock: StockJson = serde_json::from_str(&res)
-        .unwrap_or(StockJson::from(StockJson { quote: HashMap::new() }));
+    let stock: StockJsonR = serde_json::from_str(&res).unwrap_or(StockJsonR::from(StockJsonR {
+        price: 0.0,
+        change_point: 0.0,
+        change_percentage: 0.0,
+        total_vol: "0".to_owned(),
+    }));
     Ok(stock)
 }
-
